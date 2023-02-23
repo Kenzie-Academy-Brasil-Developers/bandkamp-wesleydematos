166b4b8a5069293997594e5f8b1a8c048a798b1d
wesleydematos <wesleydematos3@gmail.com>
2023-02-23 17:42:47 -0300

    chore: add  documentation route

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -45,6 +45,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "drf_spectacular",
 ]
 
 MY_APPS = [
@@ -131,6 +132,14 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 2,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Band Kamp',
+    'DESCRIPTION': 'API de músicas.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
 
 # Internationalization
